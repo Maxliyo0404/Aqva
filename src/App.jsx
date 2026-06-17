5e0e9f446f4751b88a1863155d337e54c2a1bf1f
Mahliyo <gasandigital13@gmail.com>
2026-06-17 21:44:33 +0500

    Aqva star header

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from 'react'
+import Header from "./Components/Header/Header";
+function App() {
+  return (
+    <>
+    <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,43 @@
+import { useTranslation } from "react-i18next";
+import "./Header.css";
+import React from 'react'
+
+function Header() {
+    const {t} = useTranslation();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+    const handleChangeLanguage = (event) =>{
+        i18n.changeLanguage(event.target.value)
+    };
+  return (
+    <div className="header">
+        <div className="container">
+            <div className="header-container">
+                <a className="logo" href="#"><img src={logoPages} alt="rasm" /></a>
+                 <nav className={`header-nav ${isMenuOpen ? "active" : ""}`}>
+                <ul className="header-list">
+                    <li><Link className="header-link" to="home">{t("header.home")}</Link></li>
+                    <li><Link className="header-link" to="services">{t("header.services")}</Link></li>
+                </ul>
+                <div className="header-actions">
+                    <select className="select" onChange={handleChangeLanguage} value={i18n.language}>
+                      <option value="uz">Uz</option>
+                      <option value="en">Eng</option>
+                      <option value="ru"> Rus</option>
+                    </select>
+                  </div>
+           </nav>
+      <div className={`burger-menu ${isMenuOpen ? "active" : ""}`} onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
