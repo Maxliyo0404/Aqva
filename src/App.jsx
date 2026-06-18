5e2260abde4e911f4403d34ad42100c668573fd7
Mahliyo <gasandigital13@gmail.com>
2026-06-18 10:22:01 +0500

    sectionOne star

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import React from 'react'
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Main/Hero/Hero";
+import SectionOne from "./Components/Main/SectionOne/SectionOne";
 function App() {
   return (
     <>
     <Header/>
     <Hero/>
+    <SectionOne/>
     </>
   )
 }
