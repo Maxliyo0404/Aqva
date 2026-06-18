3d45e237b766acc37f1ad35344a2ed93196501eb
Mahliyo <gasandigital13@gmail.com>
2026-06-18 10:57:10 +0500

    one swiper

--- src/Components/Main/SectionOne/SectionOne.jsx
+++ src/Components/Main/SectionOne/SectionOne.jsx
@@ -1,6 +1,9 @@
 import { useTranslation } from "react-i18next";
 import "./SectionOne.css";
 import React from 'react'
+import slider1 from "./image/slider1.jpg";
+import slider2 from "./image/slider2.jpg";
+import slider3 from "./image/slider3.jpg";
 
 function SectionOne() {
     const {t, i18n} = useTranslation();
