f5804e37a3449b10f517f4a420187961c40946ce
Mahliyo <gasandigital13@gmail.com>
2026-06-18 12:19:07 +0500

    sectionTwo star

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Main/Hero/Hero";
 import SectionOne from "./Components/Main/SectionOne/SectionOne";
+import SectionTwo from "./Components/Main/SectionTwo/SectionTwo";
 function App() {
   return (
     <>
     <Header/>
     <Hero/>
     <SectionOne/>
+    <SectionTwo/>
     </>
   )
 }
---
+++ src/Components/Main/SectionTwo/SectionTwo.jsx
@@ -0,0 +1,37 @@
+import { useTranslation } from "react-i18next";
+import "./SectionTwo.css";
+import React from 'react'
+import aqvo from "./image/aqvo.png";
+// Bu tepadagi rasmlar
+import two11 from "./image/two11.jpg";
+import two12 from "./image/two12.jpg";
+import two13 from "./image/two13.jpg";
+import two14 from "./image/two14.jpg";
+import two15 from "./image/two15.jpg";
+import two16 from "./image/two16.jpg";
+import two17 from "./image/two17.jpg";
+import two18 from "./image/two18.jpg";
+
+// Bu pastdagi rasmlar
+
+import two21 from "./image/two11.jpg";
+import two22 from "./image/two12.jpg";
+import two23 from "./image/two13.jpg";
+import two24 from "./image/two14.jpg";
+import two25 from "./image/two15.jpg";
+import two26 from "./image/two16.jpg";
+import two27 from "./image/two17.jpg";
+import two28 from "./image/two18.jpg";
+
+function SectionTwo() {
+    const {t, i18n} = useTranslation();
+  return (
+    <div className="sectionTwo">
+        <div className="container">
+            
+        </div>
+    </div>
+  )
+}
+
+export default SectionTwo
