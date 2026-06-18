9d1034ddf9e6fad52cbbc82d9538abd3bd7527d8
Mahliyo <gasandigital13@gmail.com>
2026-06-18 10:20:51 +0500

    sectionOne star

---
+++ src/Components/Main/SectionOne/SectionOne.jsx
@@ -0,0 +1,16 @@
+import { useTranslation } from "react-i18next";
+import "./SectionOne.css";
+import React from 'react'
+
+function SectionOne() {
+    const {t, i18n} = useTranslation();
+  return (
+    <div className="sectionOne" >
+        <div className="container">
+            
+        </div>
+    </div>
+  )
+}
+
+export default SectionOne
