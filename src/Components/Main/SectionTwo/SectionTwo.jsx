8e685977c71c2171cb3657e57cbf4e426150f973
Mahliyo <gasandigital13@gmail.com>
2026-06-18 13:41:23 +0500

    sectionTwo star css responsive

--- src/Components/Main/SectionTwo/SectionTwo.jsx
+++ src/Components/Main/SectionTwo/SectionTwo.jsx
@@ -5,13 +5,11 @@ import "./SectionTwo.css";
 
 import aqvo from "./image/aqvo.png";
 
-// Tepadagi rasmlar
 import two11 from "./image/two11.jpg"; import two12 from "./image/two12.jpg";
 import two13 from "./image/two13.jpg"; import two14 from "./image/two14.jpg";
 import two15 from "./image/two15.jpg"; import two16 from "./image/two16.jpg";
 import two17 from "./image/two17.jpg"; import two18 from "./image/two18.jpg";
 
-// Pastki rasmlar
 import two21 from "./image/two21.jpg"; import two22 from "./image/two22.jpg";
 import two23 from "./image/two23.jpg"; import two24 from "./image/two24.jpg";
 import two25 from "./image/two25.jpg"; import two26 from "./image/two26.jpg";
