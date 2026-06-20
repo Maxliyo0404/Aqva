07d16d09c4ac549c9ca7c9201d371af2c0cff55e
Mahliyo <gasandigital13@gmail.com>
2026-06-20 22:30:48 +0500

    sectionThree css

--- src/Components/Main/SectionThree/SectionThree.jsx
+++ src/Components/Main/SectionThree/SectionThree.jsx
@@ -21,8 +21,8 @@ function SectionThree() {
                     <div className="input-group">
                     <label htmlFor="text">{t(SectionThree.label1)}</label>
                     <input type="text" required placeholder="{t(SectionThree.input1)}" />
-                   </div>
-                    <div className="input-group">
+                
+                
                     <label htmlFor="name">{t(SectionThree.label2)}</label>
                     <input type="name" required placeholder="{t(SectionThree.input2)}" />
                    </div>
