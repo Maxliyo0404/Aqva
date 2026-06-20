13d643ee99d98105f172725d427af31380a36d14
Mahliyo <gasandigital13@gmail.com>
2026-06-20 19:46:35 +0500

    sectionThree star

--- src/Components/Main/SectionThree/SectionThree.jsx
+++ src/Components/Main/SectionThree/SectionThree.jsx
@@ -5,7 +5,19 @@ import React from 'react'
 function SectionThree() {
     const {t, i18n} = useTranslation();
   return (
-    <div>SectionThree</div>
+    <div className="sectionThree">
+        <div className="container">
+            <div className="sectionThree-werapper">
+                <div className="three-left">
+                    <h2 className="sectionThree-title">{t("sectionThree.three-title")}</h2>
+                    <p className="sectionThree-text">{t("sectionThree.three-text")}</p>
+                    <p className="sectionThree-text">{t("sectionThree.three-text")}</p>
+                    <p className="sectionThree-text">{t("sectionThree.three-text")}</p>
+        
+            </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
