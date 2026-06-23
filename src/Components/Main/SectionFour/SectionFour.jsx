9222ecefb99fc98e6fd6ed055ac707ea7621c34f
Mahliyo <gasandigital13@gmail.com>
2026-06-23 23:07:44 +0500

    sectionFour

--- src/Components/Main/SectionFour/SectionFour.jsx
+++ src/Components/Main/SectionFour/SectionFour.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import four1 from "./image/four1.png";
 import four2 from "./image/four2.png";
 import four3 from "./image/four3.png";
-import four2 from "./image/four2.png";
+import four2 from "./image/four4.png";
 
 function SectionFour() {
   const {t, i18n} = useTranslation();
