bde041b1f022c9a0cf5f64d91012637032cc230a
Mahliyo <gasandigital13@gmail.com>
2026-06-23 22:50:50 +0500

    sectionFour

--- src/Components/Main/SectionFour/SectionFour.jsx
+++ src/Components/Main/SectionFour/SectionFour.jsx
@@ -3,11 +3,13 @@ import "./SectionFour.css";
 import React from 'react'
 
 function SectionFour() {
-  const {t, i18n} =useTranslation();
+  const {t, i18n} = useTranslation();
   return (
     <div className="sectionFour">
       <div className="container">
-        <div className="four-wrapper"></div>
+        <div className="four-wrapper">
+
+        </div>
       </div>
     </div>
   )
