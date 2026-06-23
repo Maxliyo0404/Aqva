a786a4846ee1d89067266f85972bab0ab793dae3
Mahliyo <gasandigital13@gmail.com>
2026-06-23 22:48:35 +0500

    sectionFour

--- src/Components/Main/SectionFour/SectionFour.jsx
+++ src/Components/Main/SectionFour/SectionFour.jsx
@@ -1,8 +1,9 @@
+import { useTranslation } from "react-i18next";
 import "./SectionFour.css";
 import React from 'react'
 
 function SectionFour() {
-  const {t, i18n} =
+  const {t, i18n} =useTranslation
   return (
     <div>SectionFour</div>
   )
